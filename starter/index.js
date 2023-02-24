42e6f37e7ab0b40557ba3ce3b02739cb6cf7d5ed
unknown <amarmalde97@gmail.com>
2023-02-24 19:44:58 +0000

    added in constructors and started the inquerier prompts

--- starter/index.js
+++ starter/index.js
@@ -8,8 +8,42 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+
 const render = require("./src/page-template.js");
+//call render(myArrayOfTeamMembers)
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+
+const teamArray = [];
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: "What is the manager's name?",
+        name: 'name'
+    },
+    {
+        type: 'input',
+        message: "What is the manager's ID?",
+        name: 'employeeID'
+    },
+    {
+        type: 'input',
+        message: "What is the manager's email address?",
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'What is the office number?',
+        name: 'officeNumber'
+    }
+
+]).then(response => {
+    console.log(response)
+
+    manager = new Manager(response.name, response.employeeID, response.email, response.officeNumber)
+
+    teamArray.push(manager)
+})
